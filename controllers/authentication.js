7e25241e8b09b20965b61554e6d455c14b74a940
Chih-Wei <chihweiliu1993@gmail.com>
2019-01-26 12:06:13 +0100

    initial

---
+++ controllers/authentication.js
@@ -0,0 +1,37 @@
+const User = require("../models/user");
+
+exports.signup = (req, res, next) => {
+  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // see if user with a given email exists
+  User.findOne({ email: email }).then((existingUser) => {
+    // if a user with email does exist, return error
+    if(existingUser) {
+      return res.status(400).send({ message: "Email is in use" });
+    }
+    // if a user with email does not exist, create and save user record, passing in object of user detail
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save().then((err) => {
+      // send back a JWT token on successful signup
+      const token = user.generateAuthToken();
+      res.json({ token: token });
+
+    }).catch((err) => {
+      return res.status(400).send(err);
+    });
+
+  }).catch((err) => res.status(400).send(err));
+
+};
+
+exports.signin = (req, res, next) => {
+  const user = req.user;
+  const token = user.generateAuthToken();
+  res.send({ token: token });
+}
